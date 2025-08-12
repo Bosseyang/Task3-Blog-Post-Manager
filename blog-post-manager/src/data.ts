4187470f669e01b6a35f840f5c858c0fe66cc7e0
Bosseyang <bosse_yang@hotmail.com>
2025-08-12 11:05:26 +0200

    Create data.ts
    Created initial data, may add more later

---
+++ blog-post-manager/src/data.ts
@@ -0,0 +1,12 @@
+import type { IPost } from "./types";
+
+export const dummyPosts: IPost[] = [
+  {
+    id: "1",
+    title: "New post",
+    author: "1",
+    content: "Test post",
+    // timestamp: new Date(),
+    timestamp: 2025,
+  },
+];
