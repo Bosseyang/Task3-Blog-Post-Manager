23bd08250dd1be8f9f423638d1557717b456d7c0
Bosseyang <bosse_yang@hotmail.com>
2025-08-14 16:32:45 +0200

    Create handleFormSubmit.ts
    refactoring and moving handleFormSubmit function out from main

---
+++ blog-post-manager/src/ts/handleFormSubmit.ts
@@ -0,0 +1,30 @@
+import { v4 as generateId } from "uuid";
+import { postListEl, posts, savePosts, renderPosts, formEl } from "../main";
+import type { IPost } from "../types";
+import { createNewPostEl } from "./createNewPost";
+
+export function handleFormSubmit(e: Event): void {
+  e.preventDefault();
+  const inputTitleEl = document.querySelector<HTMLInputElement>("#title")!;
+  const inputAuthorEl = document.querySelector<HTMLInputElement>("#author")!;
+  const inputContentEl =
+    document.querySelector<HTMLTextAreaElement>("#content")!;
+
+  const newPost: IPost = {
+    id: generateId(),
+    title: inputTitleEl.value,
+    author: inputAuthorEl.value,
+    content: inputContentEl.value,
+    timestamp: Date.now(),
+  };
+
+  const newPostEl = createNewPostEl(newPost);
+  postListEl.insertAdjacentElement("afterbegin", newPostEl);
+
+  posts.unshift(newPost);
+  savePosts();
+  renderPosts();
+  if (formEl) {
+    formEl.reset();
+  }
+}
