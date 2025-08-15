dd88ecf48a6e1eb36f1752a8bcb6e0db24302ece
Bosseyang <bosse_yang@hotmail.com>
2025-08-15 16:04:38 +0200

    Update main.ts
    Added toggle buttons for the form to close and open.

--- blog-post-manager/src/main.ts
+++ blog-post-manager/src/main.ts
@@ -8,17 +8,33 @@ postsContainer.innerHTML = /*html*/ `
   <section class="post-list"></section>
 `;
 
-export const formEl = document.querySelector<HTMLFormElement>(".form");
+export const formEl = document.querySelector<HTMLFormElement>(".form")!;
 export const postListEl = document.querySelector<HTMLElement>(".post-list")!;
 export const filterAuthorInput =
   document.querySelector<HTMLInputElement>("#filterAuthor")!;
 export const sortSelect = document.querySelector<HTMLSelectElement>("#sort")!;
+export const formContainerEl =
+  document.querySelector<HTMLElement>("#form-container");
+export const insidePostBtn =
+  document.querySelector<HTMLParagraphElement>(".inside-post-btn")!;
+const addNewPostBtn =
+  document.querySelector<HTMLButtonElement>(".add-new-post-btn")!;
+export const closeFormBtn =
+  document.querySelector<HTMLButtonElement>(".close-form-btn")!;
 
-formEl?.addEventListener("submit", handleFormSubmit);
+addNewPostBtn.addEventListener("click", () => {
+  formContainerEl?.classList.toggle("hidden");
+});
+closeFormBtn.addEventListener("click", () => {
+  formContainerEl?.classList.toggle("hidden");
+});
+
+// closeFormBtn.addEventListener("click", (e) => handleOnClick(e));
+// addNewPostBtn.addEventListener("click", (e) => handleOnClick(e));
+
+formEl.addEventListener("submit", handleFormSubmit);
 postListEl.addEventListener("click", (e) => handleOnClick(e));
 sortSelect.addEventListener("change", renderPosts);
 filterAuthorInput.addEventListener("input", renderPosts);
 // populatePostListWithDummys();
 renderPosts();
-
-// ################### Functions below #################
