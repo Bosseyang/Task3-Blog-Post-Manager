c2fa8ee9f4a4cd7dade861794147aa77d82a6a00
Bosseyang <bosse_yang@hotmail.com>
2025-08-15 09:03:33 +0200

    Update main.ts
    Moved renderPosts to it's own file

--- blog-post-manager/src/main.ts
+++ blog-post-manager/src/main.ts
@@ -39,27 +39,6 @@ export function savePosts() {
   localStorage.setItem("posts", JSON.stringify(posts));
 }
 
-export function renderPosts() {
-  postListEl.innerHTML = "";
-
-  const filterValue = filterAuthorInput.value.toLowerCase();
-  const sortedPosts = [...posts].sort((a, b) => {
-    if (sortSelect.value === "date") {
-      return b.timestamp - a.timestamp;
-    } else {
-      return a.author.localeCompare(b.author);
-    }
-  });
-
-  const filteredPosts = sortedPosts.filter((p) =>
-    p.author.toLowerCase().includes(filterValue)
-  );
-
-  filteredPosts.forEach((post) => {
-    postListEl.appendChild(createNewPostEl(post));
-  });
-}
-
 function handleOnClick(event: MouseEvent): void {
   const target = event.target;
 
