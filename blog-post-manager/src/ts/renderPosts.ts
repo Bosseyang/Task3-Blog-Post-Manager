64a105dbc07228a55926604f71b2fcc1eef3a2d8
Bosseyang <bosse_yang@hotmail.com>
2025-08-15 09:03:55 +0200

    Create renderPosts.ts

---
+++ blog-post-manager/src/ts/renderPosts.ts
@@ -0,0 +1,23 @@
+import { postListEl, sortSelect, filterAuthorInput, posts } from "../main";
+import { createNewPostEl } from "./createNewPost";
+
+export function renderPosts() {
+  postListEl.innerHTML = "";
+
+  const filterValue = filterAuthorInput.value.toLowerCase();
+  const sortedPosts = [...posts].sort((a, b) => {
+    if (sortSelect.value === "date") {
+      return b.timestamp - a.timestamp;
+    } else {
+      return a.author.localeCompare(b.author);
+    }
+  });
+
+  const filteredPosts = sortedPosts.filter((p) =>
+    p.author.toLowerCase().includes(filterValue)
+  );
+
+  filteredPosts.forEach((post) => {
+    postListEl.appendChild(createNewPostEl(post));
+  });
+}
